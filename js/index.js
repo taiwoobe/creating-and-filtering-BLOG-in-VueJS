173642a0b49210c903e1ffffd1deeb331aa1ce53
Taiwo Ibidapo-Obe <Ibidapo-Obe>
2018-04-05 17:01:19 +0100

    Use of real life Authors & Titles

--- js/index.js
+++ js/index.js
@@ -1,74 +1,82 @@
 new Vue({
   el: '#app',
-  data: function data() {
+  data() {
     return {
       selected: '',
-      posts: [{
-        author: '@vFitzgerald',
-        title: 'Quod Ducimus Omnis',
-        category: 'science'
-      }, {
-        author: '@mPalmer',
-        title: 'Vero Eius Laboriosam Ex Repudiandae',
-        category: 'math'
-      }, {
-        author: '@mDean',
-        title: 'Magnam Odit',
-        category: 'science'
-      }, {
-        author: '@tCole',
-        title: 'Velit Mollitia Voluptates Assumenda',
-        category: 'science'
-      }, {
-        author: '@jCooper',
-        title: 'Eum Commodi Cupiditate',
-        category: 'poetry'
-      }, {
-        author: '@lLamb',
-        title: 'Amet',
-        category: 'history'
-      }, {
-        author: '@fMartin',
-        title: 'Voluptatem Fuga Asperiores Error',
-        category: 'science'
-      }, {
-        author: '@eHayes',
-        title: 'Ipsa Consectetur Repellat Quia',
-        category: 'math'
-      }, {
-        author: '@cCollier',
-        title: 'Dolor Nihil Delectus',
-        category: 'science'
-      }, {
-        author: '@cBenson',
-        title: 'Labore Ipsum Ad Pariatur',
-        category: 'poetry'
-      }],
+      posts: [
+        {
+          author: 'Jeremiah Ogbomo',
+          title: 'Nativescript for Beginners',
+          category: 'nativescript'
+        },
+        {
+          author: 'Toyin Ariyo',
+          title: 'Getting Started with React Native',
+          category: 'react'
+        },
+        {
+          author: 'Kevin Constantine',
+          title: 'Android for Experts',
+          category: 'android'
+        },
+        {
+          author: 'Bunmi Adekunle',
+          title: 'React/Redux for dummies',
+          category: 'react'
+        },
+        {
+          author: 'Lawal Afeez',
+          title: 'Swift for intermediate',
+          category: 'swift'
+        },
+        {
+          author: 'Ndubisi Anyaoku',
+          title: 'Nativescript in View',
+          category: 'nativescript'
+        },
+        {
+          author: 'Babatunde Familusi',
+          title: 'Becomming a Devops Engineer',
+          category: 'devops'
+        },
+        {
+          author: 'Lawal Afees',
+          title: 'Payment integrations in Swift',
+          category: 'swift'
+        },
+        {
+          author: 'Jerimiah Ogbomo',
+          title: 'Nativescript & Angular 4',
+          category: 'nativescript'
+        },
+        {
+          author: 'Tosin Ibidapo-Obe',
+          title: 'Implementing PayStack on Android',
+          category: 'android'
+        }
+      ],
       newTitle: '',
       newAuthor: '',
-      newCategory: ''
-    };
+      newCategory: '',
+    }
   },
-
   methods: {
-    addPost: function addPost() {
-      var addedPost = new Object({
+    addPost() {
+      let addedPost = new Object({
         author: this.newAuthor,
         title: this.newTitle,
         category: this.newCategory
       });
-      this.posts.push(addedPost);
+      this.posts.push(addedPost)
       this.newTitle = '';
       this.newAuthor = '';
       this.newCategory = '';
     }
   },
   computed: {
-    filteredByCategory: function filteredByCategory() {
-      var filter = new RegExp(this.selected, 'i');
-      return this.posts.filter(function (el) {
-        return el.category.match(filter);
-      });
+    filteredByCategory() {
+      let filter = new RegExp(this.selected, 'i')
+      return this.posts.filter(el => el.category.match(filter))
     }
   }
-});
+})
